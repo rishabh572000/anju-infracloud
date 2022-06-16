e61b16099911ffe5027568b1818256cdc03e8baf
Rishabh Kumar <rishabh.kumar@novelveritas.com>
2022-06-16 13:49:31 +0530

    Project Start

---
+++ src/Componant/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Popup from "./Popup";
+
+const Header = () =>{
+    return(
+        <>
+        <h2>Mohan</h2>
+        <Popup />
+        </>
+    )
+}
+export default Header;
